5dfa8262543175d9c672c4f94812b6a65d8fc8f0
Tomasz <mandalorian99@gmail.com>
2022-01-17 12:32:27 +0000

    search bar added

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,8 +1,29 @@
 import React from "react";
 import "./Search.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
-const Search = () => {
-  return <div>this is a search input</div>;
+const Search = (props) => {
+  const { label, searchTerm, handleInput } = props;
+
+  return (
+    <form className="search-box">
+      <input
+        type="text"
+        name="search"
+        placeholder="Search movies..."
+        value={searchTerm}
+        onInput={handleInput}
+        className="search-box__input"
+      />
+      <FontAwesomeIcon
+        icon={faSearch}
+        color="grey"
+        transform=" left-30 down-2 "
+        className="fontawesome"
+      />
+    </form>
+  );
 };
 
 export default Search;
