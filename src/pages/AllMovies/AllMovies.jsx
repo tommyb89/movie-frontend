37019439bf0e91c8b2fcf2bc2c5e7deab55566ca
Tomasz <mandalorian99@gmail.com>
2022-01-23 10:39:54 +0000

    namings

--- src/pages/AllMovies/AllMovies.jsx
+++ src/pages/AllMovies/AllMovies.jsx
@@ -3,14 +3,13 @@ import Card from "../../components/Card/Card";
 import "./AllMovies.scss";
 
 const AllMovies = (props) => {
-  const { movies } = props;
-
   return (
     <div>
       <h1 className="heading">browse all movies</h1>
 
       <div className="container">
-        {movies && movies.map((movie) => <Card key={movie.id} movie={movie} />)}
+        {props.movies &&
+          props.movies.map((movie) => <Card key={movie.id} movie={movie} />)}
       </div>
     </div>
   );
